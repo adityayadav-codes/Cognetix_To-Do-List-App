fce3f70ad001756ba73b528285cc299b935f5272
adityayadav-codes <adityayadav2905@gmail.com>
2026-06-08 16:47:01 +0530

    Initial commit

---
+++ Task.java
@@ -0,0 +1,35 @@
+public class Task {
+
+    private String description;
+    private String status;
+
+    public Task(String description) {
+
+        this.description = description;
+        this.status = "Pending";
+    }
+
+    public String getDescription(String description) {
+
+        return description;
+    }
+
+    public String  getStatus(String status) {
+
+        return status;
+    }
+
+    public void setDescription(String description) {
+
+        this.description = description;
+    }
+    public void setStatus(String status) {
+
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Task [description=" + description + ", status=" + status + "]";
+    }
+}
