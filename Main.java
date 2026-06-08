0036e5e1c1d76ae496865763f589d156ed54e95c
adityayadav-codes <adityayadav2905@gmail.com>
2026-06-08 17:16:25 +0530

    create object of TaskManager in main file and decalre switch to exectue all operations

--- Main.java
+++ Main.java
@@ -1,3 +1,54 @@
+import java.util.Scanner;
+
 public class Main {
-    
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        TaskManager manager = new TaskManager();
+
+        int choice;
+
+        while (true) {
+
+            manager.showMenu();
+
+            try {
+
+                choice = Integer.parseInt(sc.nextLine());
+
+                switch (choice) {
+
+                    case 1:
+                        manager.addTask(sc);
+                        break;
+
+                    case 2:
+                        manager.viewTasks();
+                        break;
+
+                    case 3:
+                        manager.updateTaskStatus(sc);
+                        break;
+
+                    case 4:
+                        manager.deleteTask(sc);
+                        break;
+
+                    case 5:
+                        System.out.println("Thank you for using To-Do List App!");
+                        sc.close();
+                        System.exit(0);
+
+                    default:
+                        System.out.println("Invalid choice. Please try again.");
+                }
+
+            } catch (NumberFormatException e) {
+
+                System.out.println("Please enter a valid menu option.");
+            }
+        }
+    }
 }
